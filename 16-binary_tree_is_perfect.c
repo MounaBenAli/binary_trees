78b1f24132ab25cae2c02cef4338128ffeb1ef4e
MounaBenAli <benalimouna1986@gmail.com>
2022-03-24 19:53:05 +0100

    fix checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -38,13 +38,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+	return (L_subtree == R_subtree);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	if (L_subtree == R_subtree)
-		return (1);
-
-	return (0);
 }
 
