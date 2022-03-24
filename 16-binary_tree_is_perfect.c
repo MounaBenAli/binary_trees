bee3b8ba8db53ba0584945040ce5ac9e3cd22fcd
MounaBenAli <benalimouna1986@gmail.com>
2022-03-24 19:48:08 +0100

    fix checks if a binary tree is perfect(2 child nodes and all the leaf nodes are at the same level)

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,28 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * _binary_tree_height - measures the height of a binary tree
+ * binary_tree_size - measures the size of a binary tree
  *
- * @tree : pointer to the root node of the tree to measure the height.
+ * @tree : pointer to the root node of the tree to measure the size.
  *
  * Return: If tree is NULL, return 0
  */
-size_t _binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t tree_size = 0, left_size = 0, right_size = 0;
 
 	if (!tree)
 		return (0);
 
 	if (tree)
-	{
-		left_height = _binary_tree_height(tree->left);
-		right_height = _binary_tree_height(tree->right);
-	}
+		{
+		left_size  =  binary_tree_size(tree->left);
+		right_size  =  binary_tree_size(tree->right);
+		tree_size = (left_size + right_size) + 1;
 
-	if (left_height >= right_height)
-		return (1 + left_height);
-	return (1 + right_height);
+		}
+	return (tree_size);
 }
 
 /**
@@ -34,8 +33,8 @@ size_t _binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	binary_tree_t *L_subtree = tree->left;
-	binary_tree_t *R_subtree = tree->right;
+	size_t L_subtree = binary_tree_size(tree->left);
+	size_t R_subtree = binary_tree_size(tree->right);
 
 	if (!tree && (!L_subtree || !R_subtree))
 		return (0);
@@ -43,11 +42,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	if (_binary_tree_height(L_subtree) == _binary_tree_height(R_subtree))
-	{
-		if (binary_tree_is_perfect(L_subtree) && binary_tree_is_perfect(R_subtree))
-			return (1);
-	}
+	if (L_subtree == R_subtree)
+		return (1);
+
 	return (0);
 }
 
